cb1b8c4852db613c6d751fc693d83d981cd02cb0
Sean McGill <semcgill@nextgen.com>
2016-08-05 08:48:39 -0400

    Initial ng2 app setup

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'ng2spin-demo-app',
+    template: '<div>Angular2 + Spin.js Demo App</div>'
+})
+export class AppComponent {
+
+}
