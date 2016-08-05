7d636ce2ab5569337886dc8563e37d78602c38b3
Sean McGill <semcgill@nextgen.com>
2016-08-05 09:09:00 -0400

    Setup sass

--- gulpfile.js
+++ gulpfile.js
@@ -4,3 +4,15 @@ var chalk = require('chalk');
 gulp.task('default', function(){
     console.log(chalk.blue('Running default task'));
 });
+
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+    return gulp.src('./app/app.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./app/app.scss', ['sass']);
+});
